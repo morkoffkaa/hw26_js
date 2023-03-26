d46911f27303a67a24d857289e42b87118a5bdce
Olena Kaliuzhna <116544261+morkoffkaa@users.noreply.github.com>
2023-03-26 13:49:57 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+// const myInput = document.querySelector('.myInput');
+// const newElem = document.createElement('div');
+// newElem.setAttribute('style','display:none');
+// newElem.innerHTML = ('Bonsoir Elliot');
+// myInput.after(newElem);
+
+// function makeVisible () {
+//     newElem.setAttribute('style', 'display: inline-block');
+// }
+// function hideDiv () {
+//     newElem.setAttribute('style', 'display:none');
+// }
+// myInput.addEventListener('focus',makeVisible);
+// myInput.addEventListener('blur',hideDiv);
